48916e242f054534174c43cfe871faa0f5c547cf
Ivan Mosteo <ivan.mosteo@ideamail.es>
2023-08-02 12:50:37 +0200

    crud simple

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Game;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\GameController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,8 +16,9 @@ use App\Http\Controllers\LoginController;
 */
 
 Route::get('/', function () {
+    $games = Game::all();
     if (auth()->check()) {
-        return redirect()->route('store');
+        return redirect()->route('store', ['games' => $games]);
     } else {
         return view('login/login');
     }
@@ -25,7 +28,19 @@ Route::view('/login', 'login/login')->name('login');
 Route::view('/register', 'login/register')->name('register');
 Route::view('/store', 'store/store')->middleware('auth')->name('store');
 
+Route::get('/store', [GameController::class, 'index'])->middleware('auth')->name('store');
+
+
 Route::post('/start-session', [LoginController::class, 'login'])->name('start-session');
 Route::post('/validate-register', [LoginController::class, 'register'])->name('validate-register');
 Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 
+
+Route::get('/create', [GameController::class, 'create'])->name('create-game'); 
+Route::post('/store', [GameController::class, 'store'])->name('store-game');
+
+Route::get('/edit/{game}', [GameController::class, 'editView'])->name('edit-game');
+Route::put('/edit/{game}', [GameController::class, 'update'])->name('confirm-edit-game');
+
+Route::get('/delete/{game}', [GameController::class, 'deleteView'])->name('delete-game');
+Route::delete('/delete/{game}', [GameController::class, 'delete'])->name('confirm-delete-game');
