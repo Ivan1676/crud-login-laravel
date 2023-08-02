67ef581901c8656987ae4a16613e9cb658e4f9c7
Ivan Mosteo <ivan.mosteo@ideamail.es>
2023-08-02 13:27:03 +0200

    styles navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -40,7 +40,7 @@
                 </li>
                 <li>
                     <a href="{{ route('logout') }}"
-                        class="font-medium text-primary-600 hover:underline dark:text-primary-500">Logout</a>
+                        class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Logout</a>
                 </li>
             
             </ul>
