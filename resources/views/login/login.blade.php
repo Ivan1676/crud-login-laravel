6406e516de506afc82e30f872651c1e55cd74667
Ivan Mosteo <ivan.mosteo@ideamail.es>
2023-08-02 10:10:45 +0200

    Prueba

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -2,7 +2,7 @@
 <head>
     <title>Login</title>
 </head>
-
+<!--Solo es una prueba-->
 <section class="bg-gray-50 dark:bg-gray-900">
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div
