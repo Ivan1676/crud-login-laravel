8c403a04d930ab0a633ea7c8eac7bd5d5bea2a8f
Ivan1676 <ivan.mosteo.zrg@gmail.com>
2023-08-13 22:20:20 +0200

    email working contact us

--- resources/views/contact/contact.blade.php
+++ resources/views/contact/contact.blade.php
@@ -13,14 +13,14 @@
                     email</label>
                 <input type="email" id="email" name="email"
                     class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                    value="{{ Auth::user()->email }}" readonly>
+                    value="{{ Auth::user()->email }}" readonly />
             </div>
             <div>
-                <label for="subject"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-                <input type="text" id="subject" name="subject"
+                <label for="form_subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                    Subject</label>
+                <input type="text" id="form_subject" name="form_subject"
                     class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                    placeholder="Let us know how we can help you" required>
+                    placeholder="Let us know how we can help you" required />
             </div>
             <div class="sm:col-span-2">
                 <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your
