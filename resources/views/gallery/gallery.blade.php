38f405226aaef8e6fb23d1d5e30ebacf4c9aa946
Ivan1676 <ivan.mosteo.zrg@gmail.com>
2023-08-13 17:49:07 +0200

    icon

--- resources/views/gallery/gallery.blade.php
+++ resources/views/gallery/gallery.blade.php
@@ -25,7 +25,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" />
     <link href="{{ asset('css/gallery/gallery.css') }}" rel="stylesheet" type="text/css" />
-    <link rel="icon" href="{{ asset('images/icons/icono1.ico') }}">
 </head>
 
 <body>
--- resources/views/layouts/body.blade.php
+++ resources/views/layouts/body.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Use the PNG image URL as the favicon -->
+    <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/png">
     @vite('resources/css/app.css')
 </head>
 <body class="bg-blue-950 text-white">
