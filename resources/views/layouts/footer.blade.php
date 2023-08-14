30185a4b0e20b58f2695826994d4b88e4ca67ef4
Ivan1676 <ivan.mosteo.zrg@gmail.com>
2023-08-14 20:32:14 +0200

    footer logo

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="md:flex md:justify-between">
             <div class="mb-6 md:mb-0">
                 <a href="https://flowbite.com/" class="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="FlowBite Logo" />
+                    <img src="{{ asset('images/wolf.png') }}" class="h-24 mr-3" alt="Lobo Logo" />
                     <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
                 </a>
             </div>
