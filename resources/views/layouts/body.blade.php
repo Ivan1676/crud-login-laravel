84c4cab67fef3c9f65a1daf5b4b4c96eae8f30ff
Ivan1676 <ivan.mosteo.zrg@gmail.com>
2023-08-14 18:28:18 +0200

    install flowbite (working on it)

--- resources/views/layouts/body.blade.php
+++ resources/views/layouts/body.blade.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <!-- Use the PNG image URL as the favicon -->
     <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/png">
     @vite('resources/css/app.css')
 </head>
 <body class="bg-blue-950 text-white">
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.0/flowbite.min.js"></script>
 </body>
 </html>
