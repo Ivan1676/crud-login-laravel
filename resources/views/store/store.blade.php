490c347240ba41c75734802496ddcaf3c26dbdbe
Ivan1676 <ivan.mosteo.zrg@gmail.com>
2023-08-16 21:15:22 +0200

    modal changes

--- resources/views/store/store.blade.php
+++ resources/views/store/store.blade.php
@@ -9,12 +9,10 @@
 @if ($user->role === 'admin')
     <section>
         <div class="max-w-5xl mx-auto flex items-center justify-between">
-            <!-- Add flex and justify-between to align items in a row -->
             <div>
-                <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-                    <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
-                        List Of Games Here!
-                    </span>
+                <h1
+                    class="mb-4 text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-red-700 md:text-5xl lg:text-6xl">
+                    List Of Games Here!
                 </h1>
             </div>
             <div>
@@ -64,11 +62,12 @@
     </section>
 @else
     <section class="max-w-5xl mx-auto mb-10">
-        <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-            <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+        <div>
+            <h1
+                class="mb-4 text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-red-700 md:text-5xl lg:text-6xl">
                 List Of Games Here!
-            </span>
-        </h1>
+            </h1>
+        </div>
         <form action="#" method="GET">
             <!-- Search form here -->
         </form>
@@ -81,8 +80,8 @@
                     </div>
                     <!-- Modal -->
                     <div id="gameModal_{{ $game->id }}"
-                        class="fixed inset-0 flex justify-center items-center hidden z-50">
-                        <div class="bg-black text-white p-4 rounded-lg shadow-lg w-96">
+                        class="fixed hidden inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50" name="modalGame">
+                        <div class="bg-black text-white p-4 rounded-lg shadow-lg w-96 h-auto">
                             <img src="{{ $game->cover }}" alt="Game Cover" class="w-full h-60 mb-2">
                             <h2 class="text-2xl font-bold mb-2">{{ $game->name }}</h2>
                             <div class="mb-2">Description: {{ $game->description }}</div>
@@ -90,9 +89,22 @@
                             <div class="mb-2">Release date: {{ $game->release_date }}</div>
                             <div class="mb-2">Developer: {{ $game->developer }}</div>
                             <div class="mb-2">Price: {{ $game->price }}€</div>
-                            <button class="bg-blue-500 text-white px-4 py-2 rounded-lg">Buy</button>
-                            <button class="bg-gray-300 text-gray-700 px-4 py-2 rounded-lg"
-                                onclick="toggleGameModal('{{ $game->id }}')">Close</button>
+                            <button
+                                class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-600 to-red-700 group-hover:from-red-600 group-hover:to-red-700 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800">
+                                <span
+                                    class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                    Buy Game
+                                </span>
+                            </button>
+                            <button
+                                class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                                <span
+                                    class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0"
+                                    onclick="toggleGameModal('{{ $game->id }}')">
+                                    Close
+                                </span>
+                            </button>
+
                         </div>
                     </div>
                 </div>
