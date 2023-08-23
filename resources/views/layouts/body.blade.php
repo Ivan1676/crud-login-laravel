e0879e0bb97cc223f7e0f39cfd6938f50ee3203f
Ivan Mosteo <ivan.mosteo@ideamail.es>
2023-08-23 13:52:28 +0200

    fix footer

--- resources/views/layouts/body.blade.php
+++ resources/views/layouts/body.blade.php
@@ -7,7 +7,7 @@
     <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/png">
     @vite('resources/css/app.css')
 </head>
-<body class="relative text-white bg-black">
+<body class="text-white bg-black">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.0/flowbite.min.js"></script>
 </body>
 </html>
